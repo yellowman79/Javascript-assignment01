ea3deab071896d9d4daa27c44ea002288c6cfc51
kabeer <bewarekiller7@gmail.com>
2024-02-22 03:46:16 +0500

    assignment here

---
+++ app.js
@@ -0,0 +1,83 @@
+alert("Erorr! Please enter a valid password")
+
+alert("Welcome to JS Land..\nHappy Coding!")
+
+alert("Welcome to JS Land..")
+
+alert("Happy Coding! \nPrevent this page from creating additional dialogs")
+
+alert("Hello.. I can run JS through my web browser console")
+
+var firstname = ("Kabeer");
+
+var lasttname = ("Hussain");
+
+var Email = ("bewarekiller7@gmail.com");
+
+var phonenumber = ("03408391029");
+
+var password = ("Kabeer");
+
+alert(firstname+"\n"+lasttname+"\n"+Email+"\n"+phonenumber+"\n"+password)
+
+alert("you are learning javascript")
+
+var name = "karachikings";
+alert (name)
+
+var caseqty = 144;
+var num = 9;
+alert(caseqty+num)
+
+var num = 1;
+var num = 1;
+alert(num+num)
+
+var merchtotal = 100;
+var shippingcharge = 100;
+alert (merchtotal+shippingcharge)
+
+
+var value1 = 100;
+var value2 = 100;
+
+alert(value1+value2)
+
+var age = 17;
+alert("I am"+ age +" years old"   )
+
+var time = 14;
+alert("you have visited this site"+time+" times")
+
+var date = 2007
+document.write("My birthyear is "+date)
+
+var name= "John Doe";
+var ord = "5 T-shirts";
+document.write("<br>"+name+ " ordered "+ord+"(s) on XYZ clothing store")
+
+var username = "Yellowboy";
+var myname = "kabir hussain";
+alert(username+ myname)
+
+var titledmessage = "Hello World";
+alert(titledmessage)
+
+alert("Jhone Doe")
+
+alert("15 years old")
+
+alert("Certified Mobile Application Development")
+
+alert("PIZZA"+"\n"+"PIZZ"+"\n"+"PIZ"+"\n"+"PI"+"\n"+"P")
+
+var email = "example@example.com";
+alert("My Email address is"+email)
+
+var a = "A smarter way to learn javascript"
+alert("I am trying to learn from the book "+a)
+
+document.write("<br>"+"Yah! I can write HTML content through Javascript")
+
+var last = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(last)
